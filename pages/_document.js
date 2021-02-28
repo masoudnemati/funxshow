5f850d66a173505c36b38d786974f598ecf5d38b
masoudnemati <masoud.nnr@gmail.com>
2021-02-28 12:20:59 +0330

    first commit and social media in working and I need Twitch, Aparat and donate to add

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="fa">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@latest/dist/font-face.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
